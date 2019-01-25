6226b29709125ee0dd0da30cdd05f102b7e72332
Diana Boudy <diana.boudy@gmail.com>
2019-01-25 15:04:42 +0100

    :sparkles: finish MAstermind IA

--- src/main/java/game/player/IA.java
+++ src/main/java/game/player/IA.java
@@ -1,7 +1,6 @@
 package game.player;
 
 import game.GameState;
-import game.RoundGame;
 import game.rules.Rules;
 import game.constante.GameType;
 import game.constante.GameColor;
@@ -9,6 +8,8 @@ import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class IA extends Player {
     private static final Logger myFirstLogger = LogManager.getLogger(IA.class);
@@ -28,6 +29,35 @@ public class IA extends Player {
             if (getRules().getNbColorInCode() < 10)
                 lastBadValue = 9;
         }
+    }
+
+    //generate a list of possibility until biggest color
+    private void generateAllPossibility() {
+        String tmp;
+        int nbMax = 0;
+        for (int i = 0; i < getRules().getNbEltInCode(); i++) {
+            nbMax = nbMax * 10 + getRules().getNbColorInCode();
+        }
+
+        for (int i = 0; i <= nbMax; i++) {
+            tmp = Integer.toString(i);
+            if (checkNotBadValue(tmp)) {
+                while (tmp.length() < getRules().getNbEltInCode()) {
+                    tmp = "0" + tmp;
+                }
+                allPossibility.add(tmp);
+            }
+        }
+    }
+
+    //in list of possibility, delete possibilities who contains a color which is not in rules
+    private boolean checkNotBadValue(String nb) {
+        for (int i = 0; i < nb.length(); i++) {
+            if (Character.getNumericValue(nb.charAt(i)) > getRules().getNbColorInCode()) {
+                return false;
+            }
+        }
+        return true;
     }
 
     @Override
@@ -86,7 +116,7 @@ public class IA extends Player {
             setProposition(iaProposition);
             return;
         }
-        //autres tour
+        //other turns
         for (int i = 0; i < getRules().getNbEltInCode(); i++) {
             if (getPreviousResponse().charAt(i) == '+') {
                 iaProposition += Integer.toString(getPropositionArray()[i] + 1);
@@ -100,94 +130,24 @@ public class IA extends Player {
 
     private void playMastermind() {
         Random rand = new Random();
-        String iaProposition = "";
+        String iaProposition;
 
+        //begin test all colors one by one for reduce allPossibilityList
         if (checkValidColor <= getRules().getNbColorInCode()) {
             checkValidColor(checkValidColor - 1);
             proposeUniqueColor(checkValidColor);
             checkValidColor += 1;
             return;
+        } else if (checkValidColor == getRules().getNbColorInCode() + 1) {
+            checkValidColor(checkValidColor - 1);
+            checkValidColor += 1;
         }
-        deleteBadPossibility();
-        /*if (allPossibility.size() > GameState.nbPossibleTry)
-            iaProposition = searchGoodPlace();
-        else*/
-            iaProposition = String.valueOf(allPossibility.get(0));
-        setProposition(iaProposition);
-    }
-
-    private void deleteMissingColor(String color) {
-        lastBadValue = Character.getNumericValue(color.charAt(0));
-        for (int i = 0; i < allPossibility.size(); i++) {
-            if (allPossibility.get(i).toString().contains(color)) {
-                allPossibility.remove(i);
-                i--;
-            }
-        }
-    }
-/*
-    private String searchGoodPlace() {
-
-    }*/
-
-    private void deleteBadPossibility() {
-        //supprime la précédente réponse
+        //delete previous bad possibility
         allPossibility.removeIf(i -> String.valueOf(i).equals(getProposition()));
 
-        //suprime autres mauvaises réponses liés
-         /*
-        String[] rsp = getPreviousResponse().split(",");
-        System.out.println("==========rsp : " + rsp);
-
-        getPreviousResponse();
-        getPreviousResponse();
-        if (all) {
-            for (int i = 0; i < allPossibility.size(); i++) {
-                if (allPossibility.contains(color))
-            }
-        }*/
-    }
-
-    private void generateAllPossibility() {
-        String tmp;
-        int nbMax = 0;
-        for (int i = 0; i < getRules().getNbEltInCode(); i++) {
-            nbMax = nbMax * 10 + getRules().getNbColorInCode();
-        }
-
-        for (int i = 0; i <= nbMax; i++) {
-            tmp = Integer.toString(i);
-            if (checkNotBadValue(tmp)) {
-                while (tmp.length() < getRules().getNbEltInCode()) {
-                    tmp = "0" + tmp;
-                }
-                allPossibility.add(tmp);
-            }
-        }
-    }
-
-    private boolean checkNotBadValue(String nb) {
-        for (int i = 0; i < nb.length(); i++) {
-            if (Character.getNumericValue(nb.charAt(i)) > getRules().getNbColorInCode()) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private void checkValidColor(int color) {
-        if (getPreviousResponse().equals("Missing, nothing...")) {
-            deleteMissingColor(Integer.toString(color));
-        } else {
-            String[] rsp = getPreviousResponse().split(";",2);
-            int i = 0;
-            for (String retval: rsp) {
-                if (i == 1){
-                    validColor.put(color, Character.getNumericValue(retval.charAt(0)));
-                }
-                i++;
-            }
-        }
+        //propose new possibility
+        iaProposition = String.valueOf(allPossibility.get(0));
+        setProposition(iaProposition);
     }
 
     private void proposeUniqueColor(int nb) {
@@ -197,4 +157,52 @@ public class IA extends Player {
         }
         setProposition(iaProposition);
     }
+
+/*
+    private void deleteMissingColor(String color) {
+        lastBadValue = Character.getNumericValue(color.charAt(0));
+        for (int i = 0; i < allPossibility.size(); i++) {
+            if (allPossibility.get(i).toString().contains(color)) {
+                allPossibility.remove(i);
+                i--;
+            }
+        }
+    }*/
+
+    //check the previous result of unique color proposition to reduce list of all possibilities
+    private void checkValidColor(int color) {
+        if (getPreviousResponse().equals("Missing, nothing...")) {
+            deleteBadPossibilityGoodPlace(Integer.toString(color), 0);
+        } else {
+            String[] rsp = getPreviousResponse().split(";", 2);
+            int i = 0;
+            for (String retval : rsp) {
+                if (i == 1) {
+                    deleteBadPossibilityGoodPlace(Integer.toString(color), Integer.parseInt(retval));
+                }
+                i++;
+            }
+        }
+    }
+
+    //search if possibility contains exactly the number of color expected in good place and delete others
+    private void deleteBadPossibilityGoodPlace(String color, int nbGoodPlace) {
+        final Pattern pattern = Pattern.compile("(" + color + ")", Pattern.MULTILINE);
+        Matcher matcher;
+
+        for (int i = 0; i < allPossibility.size(); i++) {
+            matcher = pattern.matcher(allPossibility.get(i).toString());
+            int j = 0;
+            while (matcher.find()) {
+                j++;
+            }
+            if (j != nbGoodPlace) {
+                //System.out.println("remove : " + allPossibility.get(i).toString());
+                allPossibility.remove(i);
+                i--;
+            }
+        }
+    }
+
 }
+
